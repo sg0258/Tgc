36aba494fae61e688c6e7cffa4218d13397ab115
sg0258 <163406914+sg0258@users.noreply.github.com>
2024-03-14 16:50:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// window.addEventListener('scroll', function () {
+//     let header = document.querySelector('header');
+//     let scrollPosition = window.scrollY;
+//     let headerHeight = header.offsetHeight;
+  
+//     if (scrollPosition > headerHeight) {
+//       header.style.backgroundColor = '';
+//     } else {
+//       header.style.backgroundColor = 'transparent';
+//     }
+//   });
+
+
+
+
+
+function myFun() {
+    let java = document.getElementById("nav")
+    if (java.style.display ==="none"){
+        java.style.display ="block";
+    }
+    else{
+        java.style.display ="none";
+    }
+}
